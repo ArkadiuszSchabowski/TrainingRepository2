e1ce41d5af80151dadda4b9d172887af0ac3bcd5
ArkadiuszSchabowski <99754633+ArkadiuszSchabowski@users.noreply.github.com>
2023-03-20 10:21:38 +0100

    ShoppingList project ended. Form1.cs ✔

---
+++ ShoppingList/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace ShoppingList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProgressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ProgressBar1
+            // 
+            this.ProgressBar1.Location = new System.Drawing.Point(45, 9);
+            this.ProgressBar1.Name = "ProgressBar1";
+            this.ProgressBar1.Size = new System.Drawing.Size(264, 43);
+            this.ProgressBar1.TabIndex = 0;
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.ItemHeight = 31;
+            this.ListBox1.Location = new System.Drawing.Point(45, 56);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(265, 283);
+            this.ListBox1.TabIndex = 1;
+            // 
+            // Btn1
+            // 
+            this.Btn1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Btn1.Location = new System.Drawing.Point(55, 382);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(115, 43);
+            this.Btn1.TabIndex = 2;
+            this.Btn1.Text = "Dodaj";
+            this.Btn1.UseVisualStyleBackColor = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Btn2.Location = new System.Drawing.Point(176, 382);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(115, 43);
+            this.Btn2.TabIndex = 3;
+            this.Btn2.Text = "Usuń";
+            this.Btn2.UseVisualStyleBackColor = true;
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Btn3.Location = new System.Drawing.Point(70, 433);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(203, 38);
+            this.Btn3.TabIndex = 4;
+            this.Btn3.Text = "Wyczyść";
+            this.Btn3.UseVisualStyleBackColor = true;
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.TextBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TextBox1.Location = new System.Drawing.Point(45, 345);
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(265, 31);
+            this.TextBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OrangeRed;
+            this.ClientSize = new System.Drawing.Size(357, 476);
+            this.Controls.Add(this.TextBox1);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.ProgressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar ProgressBar1;
+        private System.Windows.Forms.ListBox ListBox1;
+        private System.Windows.Forms.Button Btn1;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.TextBox TextBox1;
+    }
+}
+
---
+++ ShoppingList/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShoppingList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            if (ProgressBar1.Value < 100)
+            {
+                if (TextBox1.Text.Length != 0)
+                {
+                    if (ListBox1.Items.Contains(TextBox1.Text))
+                    {
+                        DialogResult result = MessageBox.Show("Element jest już na liście. Czy chcesz go dodać ponownie?", "Ostrzeżenie", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.No)
+                        {
+                        MessageBox.Show("Element nie został dodany!", "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            TextBox1.Clear();
+                            return;
+                        }
+                    }
+                    ListBox1.Items.Add(TextBox1.Text);
+                    ProgressBar1.Value += 10;
+                    TextBox1.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Próbowano dodać pusty element!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lista jest pełna!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            int i = ListBox1.SelectedIndex;
+            if (i != -1)
+            {
+                ListBox1.Items.RemoveAt(ListBox1.SelectedIndex);
+                ProgressBar1.Value -= 10;
+            }
+            else
+            {
+                MessageBox.Show("Nie zaznaczono elementu!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            ListBox1.Items.Clear();
+            ProgressBar1.Value = 0;
+        }
+    }
+}
